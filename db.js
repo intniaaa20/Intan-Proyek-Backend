41019e3c4d5119b896d26875822293ee125fb9c6
risch24 <riskafadlun2@gmail.com>
2025-03-21 14:25:33 +0800

    Perubahan di db.js dan index.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
 user: 'postgres',
 host: 'localhost',
 database: 'ecommerce-kecil',
-password: 'postgres',
+password: 'JANGANLUPALAGI20',
 port: 5432,
 });
 module.exports = pool;
